ba7eb93e310be0600b4a1f1b7ccf013d954dc7e2
iris125 <34213467+iris125@users.noreply.github.com>
2017-12-06 20:52:25 +0800

    Create project.sql
    四张表的初始状态

---
+++ project.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE project;
+CREATE TABLE administrator(
+  admin_id text(7) not null PRIMARY KEY,
+  admin_name text,
+  admin_password text);
+INSERT INTO administrator values('a000001','admin1','100001');
+INSERT INTO administrator values('a000002','admin1','100002');
+
+CREATE TABLE teacher(
+  t_id text(7) not null PRIMARY KEY,
+  //t000001
+  t_name text,
+  t_password text,
+  introduction text DEFAULT NULL,
+  address text DEFAULT NULL,
+  maxunllnum int DEFAULT 10000,
+  maxnumOfMonth int DEFAULT 100,
+  maxtimeOfMonth int DEFAULT 10000);
+  
+CREATE TABLE student(
+  s_id text not null PRIMARYKEY;
+  //s000001
+  s_name text,
+  s_password text DEFAULT NULL,
+  introduction text DEFAULT NULL);
+  
+  CREATE TABLE reservation(
+   r_id int auto-INCREMENT PRIMARY KEY,
+   teacher text(7),
+   student text(7) DEFAULT NULL,
+   r_date date,
+   r_year int,
+   r_month int,
+   beginTime text,
+   finishTime text,
+   length int,
+   building text,
+   floor text,
+   room text,
+   distance int,
+   vaild_stu text,
+   isvaild boolean DEFAULT true)
