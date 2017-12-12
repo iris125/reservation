b2c9e2114dec7be0a7aa9b6db04c7713510caef9
iris125 <34213467+iris125@users.noreply.github.com>
2017-12-12 23:52:12 +0800

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,7 +1,7 @@
 package action;
-import connect.*;
-import java.sql.*;
-import connect.jdbc;
+
+import connect.sqlAction;
+
 public class Person {
 	Person(){		
 	}
@@ -14,14 +14,24 @@ public class Person {
 	public static String password;
 	public static String inputPassword;
 	public static boolean testRegist() {
-		connect.sqlAction.executeSQL("SELECT PASSWORD FROM" + getType(id) + "WHERE ID = " + "'id';");
+		connect.sqlAction.executeSQL("SELECT PASSWORD FROM" + getType(id) + "WHERE "+ id.charAt(0) +"_id = '"+ id +"';");
 		password = connect.sqlAction.getString("password");
 		return inputPassword == password ? true:false;
 	}
-	public static String getType(String s) {
-		if (s.charAt(0) == 's') return "student";
-		if (s.charAt(0) == 't') return "teacher";
-		if (s.charAt(0) == 'a') return "administration";
+	public static String getType(String id) {
+		if (id.charAt(0) == 's') return "student";
+		else if (id.charAt(0) == 't') return "teacher";
+		else if (id.charAt(0) == 'a') return "administration";
+		else return "wrong";
+	}
+	public static void getName() {
+		String command = "SELECT name FROM "+ getType(Person.id) +"WHERE ID = '" + Person.id+"';";
+		sqlAction.executeSQL(command);
+		//
+	}
+	public static void setInfo(String info) {
+		String command = "UPDATE "+ getType(Person.id) +"SET information ='"+info+"' WHERE ID = '" + Person.id+"';";
+		sqlAction.executeSQL(command);
 	}
 	
 }
