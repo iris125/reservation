825f4cd8a6c462b181cb36c345473993b666cecc
iris125 <34213467+iris125@users.noreply.github.com>
2017-12-12 23:51:04 +0800

    Update Administrator.java
    新的修改

--- Administrator.java
+++ Administrator.java
@@ -3,7 +3,7 @@ package action;
 import connect.sqlAction;
 
 public class Administrator extends Person{
-	public int maxVaildNum;
+	
 	public void addTeacher(String name) {
 		String id = "t";
 		for(int i = 1;i <= 6;i++) {
@@ -11,7 +11,7 @@ public class Administrator extends Person{
 			id += n;
 		}
 		String password = "666666";
-		String command = String.format("INSERT INTO teacher(t_id,t_name,t_password) values (%s,%s,%s);", id,name,password);
+		String command = String.format("INSERT INTO teacher(t_id,t_name,t_password) values ('%s','%s','%s');", id,name,password);
 		sqlAction.executeSQL(command);
 	}
 	public void addStudent(String name) {
@@ -21,10 +21,15 @@ public class Administrator extends Person{
 			id += n;
 		}
 		String password = "666666";
-		String command = String.format("INSERT INTO student(s_id,s_name,s_password) values (%s,%s,%s);", id,name,password);
+		String command = String.format("INSERT INTO student(s_id,s_name,s_password) values ('%s','%s','%s');", id,name,password);
 		sqlAction.executeSQL(command);
 	}
 	public void setMaxVaildNum(int n) {
-		this.maxVaildNum = n;
+		String command = "UPDATE setting SET MaxVaildNum = "+ n +";";
+		sqlAction.executeSQL(command);
+	}
+	public void setMinRegretDate(int n) {
+		String command = "UPDATE setting SET minRegretDate = "+ n +";";
+		sqlAction.executeSQL(command);
 	}
 }
